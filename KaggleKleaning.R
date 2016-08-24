558fb81d8a49f9c81dbc7a35346a8afc8c7357ae
Christian Holmes <ckholmes5@gmail.com>
2016-08-24 10:53:38 -0400

    Cleaned data and experimentally imputed option now available

--- KaggleKleaning.R
+++ KaggleKleaning.R
@@ -34,6 +34,44 @@ df_1['DER_mass_MMC'] = impute(df_1['DER_mass_MMC'], "random")
 df_2['DER_mass_MMC'] = impute(df_2['DER_mass_MMC'], "random")
 df_3['DER_mass_MMC'] = impute(df_3['DER_mass_MMC'], "random")
 
+drops_0 = c('DER_deltaeta_jet_jet','DER_mass_jet_jet','DER_prodeta_jet_jet','DER_lep_eta_centrality','PRI_jet_subleading_pt','PRI_jet_subleading_eta','PRI_jet_subleading_phi','PRI_jet_leading_pt', 'PRI_jet_leading_eta','PRI_jet_leading_phi')
+drops_1 = c('DER_deltaeta_jet_jet','DER_mass_jet_jet','DER_prodeta_jet_jet','DER_lep_eta_centrality','PRI_jet_subleading_pt','PRI_jet_subleading_eta','PRI_jet_subleading_phi')
+
+df_0 = df_0[ , !(names(df_0) %in% drops_0)]
+df_1 = df_1[ , !(names(df_1) %in% drops_1)]
+
+
+##df_0,df_1,df_2,df_3 are the dataframes we want to apply algorithms to
+
+
+
+#Experimental Imputation Method
+df_impute = train
+
+grouped_2_cols = c('PRI_jet_leading_pt', 'PRI_jet_leading_eta','PRI_jet_leading_phi')
+grouped_4_cols = c('DER_deltaeta_jet_jet','DER_mass_jet_jet','DER_prodeta_jet_jet','DER_lep_eta_centrality','PRI_jet_subleading_pt','PRI_jet_subleading_eta','PRI_jet_subleading_phi')
+
+for (i in 2:ncol(grouped_2)) {
+  change = as.numeric(((grouped_2[4,i] - grouped_2[3,i]) + (grouped_2[3,i] - grouped_2[2,i]))/2)
+  change_jet0 = grouped_2[2,i] - change
+  df_impute[grouped_2_cols[i-1]][is.na(df_impute[grouped_2_cols[i-1]])] = change_jet0
+}
+
+impute_0 = filter(df_impute, PRI_jet_num == 0)
+impute_1 = filter(df_impute, PRI_jet_num == 1)
+impute_23 = filter(df_impute, PRI_jet_num >= 2)
+
+for (i in 2:ncol(grouped_4)) {
+  change = as.numeric(grouped_4[4,i] - grouped_4[3,i])
+  change_jet1 = grouped_4[3,i] - change
+  change_jet0 = change_jet1 - change
+  
+  impute_0[grouped_4_cols[i-1]][is.na(impute_0[grouped_4_cols[i-1]])] = change_jet0
+  impute_1[grouped_4_cols[i-1]][is.na(impute_1[grouped_4_cols[i-1]])] = change_jet1
+  
+  
+}
+exp_impute_train = rbind(impute_0, impute_1, impute_23)
 
 
 
